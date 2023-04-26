a3c95837a962861fae08e603c13ec9ddc02c1605
Lam Tuan Kiet <lamtuankiet20122003@gmail.com>
2023-04-26 08:15:37 +0700

    function export excel

---
+++ book_store/src/GUI/icon_lib.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.ImageIcon;
+
+public class icon_lib {
+    public  ImageIcon icon_add = new ImageIcon(getClass().getResource("/icon/add.png"));
+    public  ImageIcon icon_repair = new ImageIcon(getClass().getResource("/icon/repair.png"));
+    public  ImageIcon icon_remove = new ImageIcon(getClass().getResource("/icon/remove.png"));
+    public  ImageIcon icon_export_excel = new ImageIcon(getClass().getResource("/icon/export_excel.png"));
+}
